fb5a54b90882874630835c0cc7628b5e34e3c6e3
Nayem Islam <53568454+nayem-miah@users.noreply.github.com>
2024-11-26 10:38:20 +0600

    'commited'

--- components/common/Sidebar.jsx
+++ components/common/Sidebar.jsx
@@ -1,18 +1,24 @@
-'use client'
+"use client";
 
 import { SidebarContext } from "@/providers/SidebarSwitchProvider";
+import Image from "next/image";
+import Link from "next/link";
 import { useContext } from "react";
 
-
 export default function Sidebar() {
-  const {sidebar, setSidebar} = useContext(SidebarContext)
+  const { sidebar, setSidebar } = useContext(SidebarContext);
 
   return (
     <>
-      <div className={`sidebar fixed ${sidebar} top-0 z-[999] h-screen w-full bg-btn/60 transition-all duration-500`}>
+      <div
+        className={`sidebar fixed ${sidebar} top-0 z-[999] h-screen w-full bg-btn/60 transition-all duration-500`}
+      >
         <div className="ml-auto h-screen w-[300px] bg-white px-[30px] pt-[60px] dark:bg-card">
           <div className="relative flex justify-center">
-            <button onClick={()=>setSidebar('left-[100%]')} className="group absolute left-[-78px] rounded-lg bg-white px-[15px] py-[10px] text-[26px] dark:bg-card">
+            <button
+              onClick={() => setSidebar("left-[100%]")}
+              className="group absolute left-[-78px] rounded-lg bg-white px-[15px] py-[10px] text-[26px] dark:bg-card"
+            >
               <svg
                 stroke="currentColor"
                 fill="currentColor"
@@ -27,7 +33,7 @@ export default function Sidebar() {
               </svg>
             </button>
             <div>
-              <img src="/logo.png" alt="" />
+              <Image src="/logo.png" alt="logo" width={180} height={39} />
             </div>
           </div>
           <div className="my-[30px] border-t border-[#ddd] dark:border-btn"></div>
@@ -102,7 +108,7 @@ export default function Sidebar() {
                 </p>
               </div>
               <div>
-                <img src="/sun.svg" alt="" />
+                <Image src="/sun.svg" alt="sun" width={30} height={30} />
               </div>
             </div>
           </button>
@@ -112,8 +118,8 @@ export default function Sidebar() {
                 Follow Me:
               </p>
               <div className="mt-[20px] flex gap-3">
-                <a
-                  href="https://www.facebook.com/amitbiswas1994"
+                <Link
+                  href="https://www.facebook.com/nayemmiahcu"
                   target="_blank"
                 >
                   <button className="boxShadow rounded-lg bg-white p-[10px] text-[18px] text-text transition duration-300 hover:text-btn dark:bg-btn dark:shadow-none dark:hover:text-theme">
@@ -129,8 +135,8 @@ export default function Sidebar() {
                       <path d="M279.14 288l14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.43 0 225.36 0c-73.22 0-121.08 44.38-121.08 124.72v70.62H22.89V288h81.39v224h100.17V288z"></path>
                     </svg>
                   </button>
-                </a>
-                <a href="https://github.com/amitbd599" target="_blank">
+                </Link>
+                <Link href="https://github.com/nayem-miah" target="_blank">
                   <button className="boxShadow rounded-lg bg-white p-[10px] text-[18px] text-text transition duration-300 hover:text-btn dark:bg-btn dark:shadow-none dark:hover:text-theme">
                     <svg
                       stroke="currentColor"
@@ -144,9 +150,9 @@ export default function Sidebar() {
                       <path d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3.3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3zm44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9.3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3.7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3.3 2.9 2.3 3.9 1.6 1 3.6.7 4.3-.7.7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3.7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3.7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"></path>
                     </svg>
                   </button>
-                </a>
-                <a
-                  href="https://www.linkedin.com/in/amitbiswas-tm/"
+                </Link>
+                <Link
+                  href="https://www.linkedin.com/in/nayem-miah/"
                   target="_blank"
                 >
                   <button className="boxShadow rounded-lg bg-white p-[10px] text-[18px] text-text transition duration-300 hover:text-btn dark:bg-btn dark:shadow-none dark:hover:text-theme">
@@ -162,22 +168,7 @@ export default function Sidebar() {
                       <path d="M100.28 448H7.4V148.9h92.88zM53.79 108.1C24.09 108.1 0 83.5 0 53.8a53.79 53.79 0 0 1 107.58 0c0 29.7-24.1 54.3-53.79 54.3zM447.9 448h-92.68V302.4c0-34.7-.7-79.2-48.29-79.2-48.29 0-55.69 37.7-55.69 76.7V448h-92.78V148.9h89.08v40.8h1.3c12.4-23.5 42.69-48.3 87.88-48.3 94 0 111.28 61.9 111.28 142.3V448z"></path>
                     </svg>
                   </button>
-                </a>
-                <a href="https://medium.com/@amitbd599" target="_blank">
-                  <button className="boxShadow rounded-lg bg-white p-[10px] text-[18px] text-text transition duration-300 hover:text-btn dark:bg-btn dark:shadow-none dark:hover:text-theme">
-                    <svg
-                      stroke="currentColor"
-                      fill="currentColor"
-                      strokeWidth="0"
-                      viewBox="0 0 640 512"
-                      height="1em"
-                      width="1em"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path d="M180.5,74.262C80.813,74.262,0,155.633,0,256S80.819,437.738,180.5,437.738,361,356.373,361,256,280.191,74.262,180.5,74.262Zm288.25,10.646c-49.845,0-90.245,76.619-90.245,171.095s40.406,171.1,90.251,171.1,90.251-76.619,90.251-171.1H559C559,161.5,518.6,84.908,468.752,84.908Zm139.506,17.821c-17.526,0-31.735,68.628-31.735,153.274s14.2,153.274,31.735,153.274S640,340.631,640,256C640,171.351,625.785,102.729,608.258,102.729Z"></path>
-                    </svg>
-                  </button>
-                </a>
+                </Link>
               </div>
             </div>
           </div>
