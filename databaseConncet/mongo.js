813211b063d3bcf123e2596d6560975cf4740841
Nayem Islam <53568454+nayem-miah@users.noreply.github.com>
2024-11-29 02:58:32 +0600

    'commited'

--- databaseConncet/mongo.js
+++ databaseConncet/mongo.js
@@ -1,11 +1,30 @@
+// import mongoose from "mongoose";
+
+// export async function dbConnect() {
+//   try {
+//     const conn = await mongoose.connect(String(process.env.MONGODB_URI));
+
+//     return conn;
+//   } catch (err) {
+//     console.error(err);
+//   }
+// }
+
+
+
 import mongoose from "mongoose";
 
 export async function dbConnect() {
-  try {
-    const conn = await mongoose.connect(String(process.env.MONGODB_URI));
-
-    return conn;
-  } catch (err) {
-    console.error(err);
+  if (mongoose.connection.readyState === 0) {
+    try {
+      await mongoose.connect(process.env.MONGODB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        serverSelectionTimeoutMS: 10000, // 10 seconds timeout
+      });
+      console.log("Connected to MongoDB!");
+    } catch (error) {
+      console.error("Error connecting to MongoDB:", error.message);
+    }
   }
 }
