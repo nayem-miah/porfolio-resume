a8692c5010565998317f9e7ed209abaaeb64bb12
Nayem Islam <53568454+nayem-miah@users.noreply.github.com>
2024-12-03 02:25:04 +0600

    'commited'

--- app/my-projects/page.js
+++ app/my-projects/page.js
@@ -4,7 +4,7 @@ import Image from "next/image";
 import Link from "next/link";
 
 export default async function page() {
-  const data = fetch("https://nayemjs.vercel.app/api/getProject", {
+  const data = await fetch("https://nayemjs.vercel.app/api/getProject", {
     next: { revalidate: 120 },
   });
 
