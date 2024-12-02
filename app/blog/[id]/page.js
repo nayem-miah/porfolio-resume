30651d8d69c4d9030656d4b03b0eb99244296532
Nayem Islam <53568454+nayem-miah@users.noreply.github.com>
2024-12-02 12:26:19 +0600

    'commited'

--- app/blog/[id]/page.js
+++ app/blog/[id]/page.js
@@ -177,12 +177,12 @@ export default async function page({ params }) {
 }
 
 
-export async function generateStaticParams() {
-  const blogs = await getBlogs();
+// export async function generateStaticParams() {
+//   const blogs = await getBlogs();
 
-  return blogs.map((blog) => ({
-    id: blog.id.toString(),
-    revalidate: 10, // Revalidate every 10 seconds
-  }));
-}
+//   return blogs.map((blog) => ({
+//     id: blog.id.toString(),
+//     revalidate: 10, // Revalidate every 10 seconds
+//   }));
+// }
 
