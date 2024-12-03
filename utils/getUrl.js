237dcdf4c2c8010afbb627c12d41c1215947123c
Nayem Islam <53568454+nayem-miah@users.noreply.github.com>
2024-12-04 01:23:43 +0600

    'commited'

--- utils/getUrl.js
+++ utils/getUrl.js
@@ -1,6 +1,7 @@
 // utils/getURL.js
 const IS_SERVER = typeof window === "undefined";
 export default function getURL(path) {
+
   const baseURL = IS_SERVER
     ? process.env.NEXT_PUBLIC_SITE_URL
     : window.location.origin;
