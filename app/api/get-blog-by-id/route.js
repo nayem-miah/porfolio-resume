3f4594ab564b22d5f3fb3d8a780c8f5533198515
Nayem Islam <53568454+nayem-miah@users.noreply.github.com>
2024-12-03 14:35:11 +0600

    'commited'

--- app/api/get-blog-by-id/route.js
+++ app/api/get-blog-by-id/route.js
@@ -1,42 +1,24 @@
 
-// import { blogModel } from "@/models/blog-model";
-// import { dbConnect } from "@/databaseConncet/mongo";
-// import { NextResponse } from "next/server";
-
-// export const GET = async (request) => {
-//   await dbConnect();
-
-//   // Extract the ID from query parameters or URL
-//   const { searchParams } = new URL(request.url);
-//   const id = searchParams.get("id");
-//   try {
-//     const blog = await blogModel.findById(id);
-
-//     if (!blog) {
-//       return NextResponse.json({ error: "Blog not found" }, { status: 404 });
-//     }
-
-//     return NextResponse.json(blog, { status: 200 });
-//   } catch (e) {
-//     return NextResponse.json({ error: e.message }, { status: 500 });
-//   }
-// };
-
 import { blogModel } from "@/models/blog-model";
-const { dbConnect } = require("@/databaseConncet/mongo");
-const { NextResponse } = require("next/server");
+import { dbConnect } from "@/databaseConncet/mongo";
+import { NextResponse } from "next/server";
 
-export const GET = async () => {
+export const GET = async (request) => {
   await dbConnect();
+
+  // Extract the ID from query parameters or URL
+  const { searchParams } = new URL(request.url);
+  const id = searchParams.get("id");
   try {
-    const blogs = await blogModel.find();
-    return new NextResponse(JSON.stringify(blogs), {
-      headers: { "Content-Type": "application/json" },
-    });
+    const blog = await blogModel.findById(id);
+
+    if (!blog) {
+      return NextResponse.json({ error: "Blog not found" }, { status: 404 });
+    }
+
+    return NextResponse.json(blog, { status: 200 });
   } catch (e) {
-    return new NextResponse(JSON.stringify({ error: e.message }), {
-      status: 500,
-      headers: { "Content-Type": "application/json" },
-    });
+    return NextResponse.json({ error: e.message }, { status: 500 });
   }
 };
+
