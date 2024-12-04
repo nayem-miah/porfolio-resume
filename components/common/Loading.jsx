c28fb53edcac88e0016d335ae99eca17cf424cdc
Nayem Islam <53568454+nayem-miah@users.noreply.github.com>
2024-12-04 13:38:05 +0600

    'commited'

--- components/common/Loading.jsx
+++ components/common/Loading.jsx
@@ -1,99 +1,67 @@
 export default function Loading() {
   return (
     <>
-      <div className="grid sm:grid-cols-2 xl:grid-cols-3 gap-7 animate-pulse">
-        <figure className="p-4 border border-black/10 shadow-sm dark:border-white/10 rounded-xl">
-          <div className="w-full h-48 bg-gray-200 rounded"></div>
-          <figcaption className="pt-4">
-            <div className="h-6 bg-gray-200 rounded w-1/2 mb-1"></div>
-            <div className="h-4 bg-gray-200 rounded w-1/3 mb-2"></div>
-            <div className="flex items-center space-x-1 mb-5">
-              <div className="h-3 w-3 bg-gray-200 rounded-full"></div>
-              <div className="h-3 w-3 bg-gray-200 rounded-full"></div>
-              <div className="h-3 w-3 bg-gray-200 rounded-full"></div>
-              <div className="h-3 w-3 bg-gray-200 rounded-full"></div>
-              <div className="h-3 w-3 bg-gray-200 rounded-full"></div>
+      <div className="grid sm:grid-cols-2 xl:grid-cols-2 gap-7 animate-pulse">
+        <div class="group relative mb-2 animate-pulse">
+          <div class="h-48 w-full overflow-hidden rounded-xl">
+            <div class="h-full w-full bg-gray-200"></div>
+          </div>
+          <div class="mt-8">
+            <div class="flex gap-4">
+              <div class="inline-block rounded-full bg-gray-200 px-6 py-1 text-sm"></div>
+              <div class="inline-block rounded-full bg-gray-200 px-6 py-1 text-sm"></div>
             </div>
-            <div className="bg-primary rounded-lg h-10 px-5"></div>
-          </figcaption>
-        </figure>
+            <div class="mt-2">
+              <h2 class="h-6 bg-gray-200 rounded w-3/4"></h2>
+            </div>
+          </div>
+        </div>
 
-        <figure className="p-4 border border-black/10 shadow-sm dark:border-white/10 rounded-xl">
-          <div className="w-full h-48 bg-gray-200 rounded"></div>
-          <figcaption className="pt-4">
-            <div className="h-6 bg-gray-200 rounded w-1/2 mb-1"></div>
-            <div className="h-4 bg-gray-200 rounded w-1/3 mb-2"></div>
-            <div className="flex items-center space-x-1 mb-5">
-              <div className="h-3 w-3 bg-gray-200 rounded-full"></div>
-              <div className="h-3 w-3 bg-gray-200 rounded-full"></div>
-              <div className="h-3 w-3 bg-gray-200 rounded-full"></div>
-              <div className="h-3 w-3 bg-gray-200 rounded-full"></div>
-              <div className="h-3 w-3 bg-gray-200 rounded-full"></div>
+        <div class="group relative mb-2 animate-pulse">
+          <div class="h-48 w-full overflow-hidden rounded-xl">
+            <div class="h-full w-full bg-gray-200"></div>
+          </div>
+          <div class="mt-8">
+            <div class="flex gap-4">
+              <div class="inline-block rounded-full bg-gray-200 px-6 py-1 text-sm"></div>
+              <div class="inline-block rounded-full bg-gray-200 px-6 py-1 text-sm"></div>
             </div>
-            <div className="bg-primary rounded-lg h-10 px-5"></div>
-          </figcaption>
-        </figure>
+            <div class="mt-2">
+              <h2 class="h-6 bg-gray-200 rounded w-3/4"></h2>
+            </div>
+          </div>
+        </div>
 
-        <figure className="p-4 border border-black/10 shadow-sm dark:border-white/10 rounded-xl">
-          <div className="w-full h-48 bg-gray-200 rounded"></div>
-          <figcaption className="pt-4">
-            <div className="h-6 bg-gray-200 rounded w-1/2 mb-1"></div>
-            <div className="h-4 bg-gray-200 rounded w-1/3 mb-2"></div>
-            <div className="flex items-center space-x-1 mb-5">
-              <div className="h-3 w-3 bg-gray-200 rounded-full"></div>
-              <div className="h-3 w-3 bg-gray-200 rounded-full"></div>
-              <div className="h-3 w-3 bg-gray-200 rounded-full"></div>
-              <div className="h-3 w-3 bg-gray-200 rounded-full"></div>
-              <div className="h-3 w-3 bg-gray-200 rounded-full"></div>
+        <div class="group relative mb-2 animate-pulse">
+          <div class="h-48 w-full overflow-hidden rounded-xl">
+            <div class="h-full w-full bg-gray-200"></div>
+          </div>
+          <div class="mt-8">
+            <div class="flex gap-4">
+              <div class="inline-block rounded-full bg-gray-200 px-6 py-1 text-sm"></div>
+              <div class="inline-block rounded-full bg-gray-200 px-6 py-1 text-sm"></div>
             </div>
-            <div className="bg-primary rounded-lg h-10 px-5"></div>
-          </figcaption>
-        </figure>
-        <figure className="p-4 border border-black/10 shadow-sm dark:border-white/10 rounded-xl">
-          <div className="w-full h-48 bg-gray-200 rounded"></div>
-          <figcaption className="pt-4">
-            <div className="h-6 bg-gray-200 rounded w-1/2 mb-1"></div>
-            <div className="h-4 bg-gray-200 rounded w-1/3 mb-2"></div>
-            <div className="flex items-center space-x-1 mb-5">
-              <div className="h-3 w-3 bg-gray-200 rounded-full"></div>
-              <div className="h-3 w-3 bg-gray-200 rounded-full"></div>
-              <div className="h-3 w-3 bg-gray-200 rounded-full"></div>
-              <div className="h-3 w-3 bg-gray-200 rounded-full"></div>
-              <div className="h-3 w-3 bg-gray-200 rounded-full"></div>
+            <div class="mt-2">
+              <h2 class="h-6 bg-gray-200 rounded w-3/4"></h2>
             </div>
-            <div className="bg-primary rounded-lg h-10 px-5"></div>
-          </figcaption>
-        </figure>
-        <figure className="p-4 border border-black/10 shadow-sm dark:border-white/10 rounded-xl">
-          <div className="w-full h-48 bg-gray-200 rounded"></div>
-          <figcaption className="pt-4">
-            <div className="h-6 bg-gray-200 rounded w-1/2 mb-1"></div>
-            <div className="h-4 bg-gray-200 rounded w-1/3 mb-2"></div>
-            <div className="flex items-center space-x-1 mb-5">
-              <div className="h-3 w-3 bg-gray-200 rounded-full"></div>
-              <div className="h-3 w-3 bg-gray-200 rounded-full"></div>
-              <div className="h-3 w-3 bg-gray-200 rounded-full"></div>
-              <div className="h-3 w-3 bg-gray-200 rounded-full"></div>
-              <div className="h-3 w-3 bg-gray-200 rounded-full"></div>
+          </div>
+        </div>
+
+        <div class="group relative mb-2 animate-pulse">
+          <div class="h-48 w-full overflow-hidden rounded-xl">
+            <div class="h-full w-full bg-gray-200"></div>
+          </div>
+          <div class="mt-8">
+            <div class="flex gap-4">
+              <div class="inline-block rounded-full bg-gray-200 px-6 py-1 text-sm"></div>
+              <div class="inline-block rounded-full bg-gray-200 px-6 py-1 text-sm"></div>
             </div>
-            <div className="bg-primary rounded-lg h-10 px-5"></div>
-          </figcaption>
-        </figure>
-        <figure className="p-4 border border-black/10 shadow-sm dark:border-white/10 rounded-xl">
-          <div className="w-full h-48 bg-gray-200 rounded"></div>
-          <figcaption className="pt-4">
-            <div className="h-6 bg-gray-200 rounded w-1/2 mb-1"></div>
-            <div className="h-4 bg-gray-200 rounded w-1/3 mb-2"></div>
-            <div className="flex items-center space-x-1 mb-5">
-              <div className="h-3 w-3 bg-gray-200 rounded-full"></div>
-              <div className="h-3 w-3 bg-gray-200 rounded-full"></div>
-              <div className="h-3 w-3 bg-gray-200 rounded-full"></div>
-              <div className="h-3 w-3 bg-gray-200 rounded-full"></div>
-              <div className="h-3 w-3 bg-gray-200 rounded-full"></div>
+            <div class="mt-2">
+              <h2 class="h-6 bg-gray-200 rounded w-3/4"></h2>
             </div>
-            <div className="bg-primary rounded-lg h-10 px-5"></div>
-          </figcaption>
-        </figure>
+          </div>
+        </div>
+
       </div>
     </>
   );
