5ce84e0f689fa6e72885bb9e99707bc7368772c4
Nayem Islam <53568454+nayem-miah@users.noreply.github.com>
2024-12-05 14:08:22 +0600

    'commited'

---
+++ app/api/delete-project/route.js
@@ -0,0 +1,38 @@
+
+import { dbConnect } from "@/databaseConncet/mongo"; 
+import { NextResponse } from "next/server";
+import { projectstModel } from "@/models/projects-model";
+
+export const DELETE = async (request) => { 
+  await dbConnect();
+
+  // Extract the ID from query parameters
+  const { searchParams } = new URL(request.url);
+  const id = searchParams.get("id");
+
+ 
+  if (!id) {
+    return new NextResponse("ID parameter is missing", {
+      status: 400, // Bad Request
+    });
+  }
+
+  try {
+    const result = await projectstModel.findByIdAndDelete(id);
+
+    if (!result) {
+      return new NextResponse("Project not found", {
+        status: 404, // Not Found
+      });
+    }
+
+    return new NextResponse("Project was deleted!", {
+      status: 200, 
+    });
+  } catch (error) {
+    console.error("Error deleting project:", error);
+    return new NextResponse("Internal Server Error", {
+      status: 500, // Internal Server Error
+    });
+  }
+};
